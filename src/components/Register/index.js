7549bccb0cc03eb30d38eb438acf8f2830a02447
Jr Bytes <itamazor@gmail.com>
2020-04-11 23:30:30 -0300

    Iniciado desenvolvimento web do Agent Report

---
+++ src/components/Register/index.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react' 
+
+function Register() {
+
+
+  return (
+    <div className="register-container">
+      <div className="content">
+        <section>
+          
+        </section>
+      </div>
+    </div>
+  )
+}
+
+export default Register
